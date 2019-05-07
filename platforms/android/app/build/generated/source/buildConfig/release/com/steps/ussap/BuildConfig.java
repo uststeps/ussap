f7b06d37a675843cbbc2208eec2030b36122c75d
uststeps <uststeps@users.noreply.github.com>
2019-05-07 11:16:01 +0800

    hotfixphase2

--- platforms/android/app/build/generated/source/buildConfig/release/com/steps/ussap/BuildConfig.java
+++ platforms/android/app/build/generated/source/buildConfig/release/com/steps/ussap/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.steps.ussap";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 30000;
-  public static final String VERSION_NAME = "3.0.0";
+  public static final int VERSION_CODE = 30001;
+  public static final String VERSION_NAME = "3.0.1";
 }
